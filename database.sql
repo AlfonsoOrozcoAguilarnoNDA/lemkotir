923c4038abfd7eb2a7946db9ed1109dff6f8cfbe
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-13 06:07:31 -0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,29 +1,28 @@
 CREATE TABLE `cat_users` (
 `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-`Rol` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT ‘N/A’,
+`Rol` varchar(200) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'N/A',
 `username` varchar(191) COLLATE utf8mb4_unicode_520_ci NOT NULL,
 `email` varchar(191) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-`password` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT ‘Legacy/Base password’,
-`password_2026` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT ‘Updated secure hash 2026’,
+`password` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT 'Legacy/Base password',
+`password_2026` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL COMMENT 'Updated secure hash 2026',
 `first_name` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
 `last_name` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
 `gender` char(1) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
 `headline` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
 `bio` mediumtext COLLATE utf8mb4_unicode_520_ci,
-`profile_image` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT ‘_defaultUser.png’,
+`profile_image` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '_defaultUser.png',
 `profile_blob` mediumblob,
 `profile_mime_type` varchar(50) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
 `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 `last_login_at` timestamp NULL DEFAULT NULL,
 `plano_2026` varchar(200) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-`PAPELERA` varchar(2) COLLATE utf8mb4_unicode_520_ci DEFAULT ‘NO’,
+`PAPELERA` varchar(2) COLLATE utf8mb4_unicode_520_ci DEFAULT 'NO',
 `whatsapp` varchar(100) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
 PRIMARY KEY (`id`),
 UNIQUE KEY `idx_email` (`email`),
 UNIQUE KEY `idx_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-
 -- ============================================================
 -- SQL PARA CREAR USUARIO newadmin CON PASSWORD HASHEADO
 -- ============================================================
