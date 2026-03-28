872b79415bc9b99cfd531aad2dfd4f006e70e412
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 07:34:13 -0600

    Update validate.php
    jsdelivr y 3.7.1 de jquery

--- validate.php
+++ validate.php
@@ -565,7 +565,7 @@ if ($module === 'dashboard' && !$isLoggedIn) {
 <?php endif; ?>
 
 <!-- Bootstrap 4.6 JS y jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
