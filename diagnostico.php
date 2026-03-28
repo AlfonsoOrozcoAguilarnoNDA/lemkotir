679e54d69ad1ed5b5908e77a4796884f1d28005b
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 07:32:29 -0600

    Update diagnostico.php
    jsdlivr

--- diagnostico.php
+++ diagnostico.php
@@ -361,8 +361,7 @@ function random13() {
 </footer>
 
 <!-- Bootstrap 4.6 JS y jQuery -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 </html>
