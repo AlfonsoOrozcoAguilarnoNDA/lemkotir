6995ffa4405b1b5d86f51b0451e031eee4c6981b
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 07:21:20 -0600

    Update change_password.php
    jsdlivr y 6.7.1

--- change_password.php
+++ change_password.php
@@ -144,7 +144,7 @@ if ($module === 'logout') {
     <!-- Bootstrap 4.6 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <!-- Font Awesome 5.0 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css">
     <style>
         :root {
             --gradient-start: #1a2a6c;
@@ -386,7 +386,7 @@ if ($module === 'logout') {
 </div>
 
 <!-- Bootstrap 4.6 JS y jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
