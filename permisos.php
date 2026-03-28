cc13aa60a187be1379dbe8f15e97a7905a32a836
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 07:37:10 -0600

    Update permisos.php
    jsdelivr y jquery a 3.7.1

--- permisos.php
+++ permisos.php
@@ -668,10 +668,8 @@ if ($usuario_seleccionado > 0) {
         </small>
     </div>
 </footer>
-
 <!-- Bootstrap 4.6 JS y jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 </html>
