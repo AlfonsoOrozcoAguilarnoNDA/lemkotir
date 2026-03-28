3e9538c1dbb60b0ee0d5eea9f6b576edae690f52
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 07:30:33 -0600

    Update revisabom.php
    jsdelivr

--- revisabom.php
+++ revisabom.php
@@ -592,7 +592,7 @@ foreach ($archivos_encontrados as $archivo) {
 </footer>
 
 <!-- Bootstrap 4.6 JS y jQuery -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
