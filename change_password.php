3c2088227d6512f4ab7d234694227f9bf47fe900
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-28 07:38:40 -0600

    Update change_password.php
    js delivr

--- change_password.php
+++ change_password.php
@@ -386,8 +386,7 @@ if ($module === 'logout') {
 </div>
 
 <!-- Bootstrap 4.6 JS y jQuery -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 </html>
